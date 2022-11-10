eb9636b1b0f7bf26c4853eb6ee63d2c6f3235552
NichakornS <88883675+NichakornS@users.noreply.github.com>
2022-11-10 15:33:41 +0700

    Update DirectionTest.java

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -2,37 +2,56 @@ package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * A very simple (and not particularly useful)
- * test class to have a starting point where to put tests.
+ * Test suite to confirm that {@link Unit}s correctly (de)occupy squares.
  *
- * @author Arie van Deursen
+ * @author Jeroen Roosen 
+ *
  */
-public class DirectionTest {
+class OccupantTest {
+
     /**
-     * Do we get the correct delta when moving north?
+     * The unit under test.
      */
-    @Test
-    void testNorth() {
-        Direction north = Direction.valueOf("NORTH");
-        assertThat(north.getDeltaY()).isEqualTo(-1);
-    }
-    @Test
-    void testSouth() {
-        Direction south = Direction.valueOf("SOUTH");
-        assertThat(south.getDeltaY()).isEqualTo(1);
-    }
-    @Test
-    void testWest() {
-        Direction west = Direction.valueOf("WEST");
-        assertThat(west.getDeltaX()).isEqualTo(-1);
-    }
-    @Test
-    void testEast() {
-        Direction east = Direction.valueOf("EAST");
-        assertThat(east.getDeltaX()).isEqualTo(1);
+    private Unit unit;
+
+    /**
+     * Resets the unit under test.
+     */
+    @BeforeEach
+    void setUp() {
+        unit = new BasicUnit();
     }
 
+    /**
+     * Asserts that a unit has no square to start with.
+     */
+    @Test
+    void noStartSquare() {
+        // Remove the following placeholder:
+        assertThat(unit).isNotNull();
+    }
+
+    /**
+     * Tests that the unit indeed has the target square as its base after
+     * occupation.
+     */
+    @Test
+    void testOccupy() {
+        // Remove the following placeholder:
+        assertThat(unit).isNotNull();
+    }
+
+    /**
+     * Test that the unit indeed has the target square as its base after
+     * double occupation.
+     */
+    @Test
+    void testReoccupy() {
+        // Remove the following placeholder:
+        assertThat(unit).isNotNull();
+    }
 }
